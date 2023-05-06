8ff4121d43a20a4603ff957de7a17799b4c55e58
mxstoto6 <mxstotolatomate@gmail.com>
2023-05-06 18:09:43 +0200

    Initialized vue-router

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero numquam repudiandae tenetur laudantium nihil.</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+import './style.css'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
