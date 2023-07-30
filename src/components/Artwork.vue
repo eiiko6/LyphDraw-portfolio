647a5411633ef899fe62d4bdbbb6b8a5943ec818
mxstoto6 <mxstotolatomate@gmail.com>
2023-07-30 20:19:22 +0200

    fixed css issues in artwork component

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="content">
         <div class="card">
-                <a :href="file" target="_blank">
-                    <div class="card-bg" :style="'background-image: url(' + preview + ');'"></div>
+            <a :href="file" target="_blank">
+                <div class="card-bg" :style="'background-image: url(' + preview + ');'"></div>
 
-                    <div class="card-content">
-                        <div class="popup">
+                <div class="card-content">
+                    <div class="popup">
                         <h2>Click to see</h2>
-                        </div>
-
-                        <div class="true-content">
-                            <h2>{{ name }}</h2>
-                            <p>{{ description }}</p>
-                        </div>
                     </div>
-                </a>
-            </div>
+
+                    <div class="true-content">
+                        <h2>{{ name }}</h2>
+                        <p>{{ description }}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -32,6 +32,50 @@ export default {
 </script>
 
 <style scoped>
+
+    .card:hover {
+        transform: scale(1.01);
+    }
+    .card:hover .card-bg {
+        transform: scale(1.2);
+        filter: brightness(1) blur(0);
+    }
+    .card:hover .true-content {
+        transition: .2s ease-out;
+        opacity: 0;
+    }
+
+    .card:hover .popup {
+        opacity: 1;
+        transform: scale(2);
+    }
+
+    .card {
+        position: relative;
+        width: 400px;
+        height: 500px;
+        border: 2px solid #ffffff80;
+        border-radius: 15px;
+        margin: 20px;
+        align-self: flex-start;
+        overflow: hidden;
+        font-size: 25px;
+        transition: .2s ease-out;
+    }
+
+    .card-bg {
+        transition: .2s ease-out;
+        z-index: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-size: cover;
+        background-position: center;
+        filter: brightness(0.3) blur(3px);
+    }
+
     .card-content {
         position: relative;
         transition: .2s ease-out;
@@ -68,10 +112,6 @@ export default {
     .link:focus,
     .link:focus-visible {
     outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    .button {
-        margin-top: 40px;
     }
 
     img {
--- src/pages/Artworks.vue
+++ src/pages/Artworks.vue
@@ -7,9 +7,9 @@
 
         <h1 class="title">Official artworks</h1>
 
-        <div class="cards" v-for="artwork in artworks" :key="artwork.id">
+        <div class="cards">
 
-            <Artwork class="card" :name="artwork.name" :description="artwork.description" :preview="artwork.preview" :file="artwork.file"/>
+            <Artwork :name="artworks[0].name" :description="artworks[0].description" :preview="artworks[0].preview" :file="artworks[0].file"/>
 
             <div class="footer"></div>
 
@@ -105,108 +105,4 @@ export default {
         flex-wrap: wrap;
         margin-top: 50px;
     }
-
-    .card:hover {
-        transform: scale(1.01);
-    }
-    .card:hover .card-bg {
-        transform: scale(1.2);
-        filter: brightness(1) blur(0);
-    }
-    .card:hover .true-content {
-        transition: .2s ease-out;
-        opacity: 0;
-    }
-
-    .card:hover .popup {
-        opacity: 1;
-        transform: scale(2);
-    }
-
-    .card {
-        position: relative;
-        width: 400px;
-        height: 500px;
-        border: 2px solid #ffffff80;
-        border-radius: 15px;
-        margin: 20px;
-        align-self: flex-start;
-        overflow: hidden;
-        font-size: 25px;
-        transition: .2s ease-out;
-    }
-
-    .card-bg {
-        transition: .2s ease-out;
-        z-index: 0;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-size: cover;
-        background-position: center;
-        filter: brightness(0.3) blur(3px);
-    }
-
-    .card-content {
-        position: relative;
-        transition: .2s ease-out;
-    }
-
-    .card-content .true-content {
-        margin-top: 15%;
-    }
-
-    .popup {
-        margin-top: 55%;
-        transition: .2s ease-out;
-        opacity: 0;
-    }
-
-    .link {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #2e2e2e;
-        cursor: pointer;
-        transition: border-color 0.25s;
-        color: #ff8080;
-    }
-
-    .link:hover {
-        border-color: #ff646497;
-        color: #ff6464;
-    }
-
-    .link:focus,
-    .link:focus-visible {
-    outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    .button {
-        margin-top: 40px;
-    }
-
-    img {
-        height: 150px;
-    }
-
-    .title {
-        margin-left: 30px;
-        margin-right: 30px;
-    }
-
-    h2 {
-        margin: 0;
-        margin-bottom: 15px;
-    }
-
-    p {
-        margin: 0;
-    }
-
 </style>
