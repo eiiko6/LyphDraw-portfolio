bec3b92b35ab159401ea94c7b8ada5e6c9bf0b5c
mxstoto6 <mxstotolatomate@gmail.com>
2023-08-03 10:33:12 +0200

    styled settings button

--- src/components/Language_switch.vue
+++ src/components/Language_switch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-        <button>Lang: {{ lang }}</button>
+        <button><p>Lang: {{ lang }}</p></button>
     </div>
 </template>
 
@@ -13,5 +13,33 @@
 </script>
 
 <style scoped>
+    button {
+        transition: ease-out .2s;
+        background: none;
+        color: #ffffffde;
+        border-radius: 0;
+        border: none;
+        border-bottom-left-radius: 8px;
+        border-bottom: solid #ffffffde 1px;
+        border-left: solid #ffffffde 1px;
+    }
 
+    button:hover {
+        border: none;
+        border-bottom: solid #ffffffde 1px;
+        border-left: solid #ffffffde 1px;
+        background-color: #ffffff1e;
+    }
+
+    p {
+        transition: ease-out .2s;
+        margin: 0;
+        height: fit-content;
+        width: fit-content;
+        top: 0;
+    }
+
+    button:hover p {
+        transform: translateY(-4px);
+    }
 </style>
